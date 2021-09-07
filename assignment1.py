6356bbb9c919f2811923eaaec1cc2ba402fdcb68
Pavithra-ks <90048190+Pavithra-ks@users.noreply.github.com>
2021-09-07 20:26:28 +0530

    Assignment 1 submitted

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+# add two numbers
+num1 = int(input('Enter first no:'))
+num2 = int(input('Enter second no:'))
+sum = num1 + num2
+print(f"The addition of {num1} and {num2} is {sum}")
+
+# Try to add integer with string and see the output
+a = '4'
+b = 6
+c = a + b
+print(c)  # we will get an error in the output
+
+# Print variables and strings in one line with the help of f string
+Name ='Pavithra'
+Age = 18
+print(f"My name is {Name} and i m {Age} years old.")
+
+# How to add space and new line in the print statement
+print("Do what u \t believe in :)") # \t - to add space
+print("Do what u \nbelieve in :)")  # \n - for a new line
